e41f6ff3053574cd000baaa36d47d12dca7cd29e
AlekseiKostromin <cs777@yandex.ru>
2023-08-07 19:04:27 +0500

    add def my_function_minus

--- funcs.py
+++ funcs.py
@@ -1,4 +1,14 @@
 def my_function_plas():
     sum_plas = 5 + 6
     print(sum_plas)
+
+
 my_function_plas()
+
+def my_funcion_minus():
+    sum_minus = 11 - 2
+    print(sum_minus)
+my_funcion_minus()
+
+
+
